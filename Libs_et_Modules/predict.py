797664c9fa70d97d64ff80739ef5b512c04c5d2e
Tung Nguyen <tung18tht@gmail.com>
2017-05-15 10:42:57 +0200

    Translate to English

--- Libs_et_Modules/predict.py
+++ Libs_et_Modules/predict.py
@@ -9,11 +9,11 @@ def clearLine(line):
     return line.lower()
 
 def unigrams(infile, outfile='unigrams.txt'):
-    print('Calcul des unigrammes...', end='', flush=True)
+    print('Calculate the unigrams...', end='', flush=True)
     unigrams = {}
     try:
-        with open(infile, 'r') as fic:
-            for line in fic:
+        with open(infile, 'r') as file:
+            for line in file:
                 for word in clearLine(line).split(' '):
                     if word != '':
                         if word in unigrams:
@@ -21,17 +21,17 @@ def unigrams(infile, outfile='unigrams.txt'):
                         else:
                             unigrams[word] = 1
     except Exception as e:
-        print('\nUne erreur est survenue lors de la lecture de', infile)
+        print('\nAn error occurred while reading ', infile)
         print(e.strerror)
         exit(1)
 
     try:
-        with open(outfile, 'w') as fic:
+        with open(outfile, 'w') as file:
             unigrams = collections.OrderedDict(sorted(unigrams.items(), key=lambda t : t[1], reverse=True))
             for word in unigrams:
-                fic.write(word + ' ' + str(unigrams[word]) + '\n')
+                file.write(word + ' ' + str(unigrams[word]) + '\n')
     except Exception as e:
-        print('\nUne erreur est survenue lors de l\'écriture dans', outfile)
+        print('\nAn error occurred while writing ', outfile)
         print(e.strerror)
         exit(1)
 
@@ -39,17 +39,17 @@ def unigrams(infile, outfile='unigrams.txt'):
 
 def ngrams(infile, n, outfile=''):
     if n < 2:
-        print('Fonction ngrams() : Valeur minimale de n : 2')
+        print('Function ngrams(): Minimum value of n: 2')
         exit(3)
     if outfile == '':
         outfile = str(n) + '_grams.txt'
-    print('Calcul des {}-grammes...'.format(n), end='', flush=True)
+    print('Calculate the {}-grams...'.format(n), end='', flush=True)
     ngrams = {}
     try:
-        with open(infile, 'r') as fic:
-            for line in fic:
+        with open(infile, 'r') as file:
+            for line in file:
                 words = clearLine(line).split(' ')
-                words = [ elt for elt in words if elt != '' ]
+                words = [ element for element in words if element != '' ]
                 words = [ ' '.join(name) for name in zip(*[words[i:] for i in range(n)])]
                 for word in words:
                     if word in ngrams:
@@ -57,29 +57,29 @@ def ngrams(infile, n, outfile=''):
                     else:
                         ngrams[word] = 1
     except Exception as e:
-        print('\nUne erreur est survenue lors de la lecture de', infile)
+        print('\nAn error occurred while reading ', infile)
         print(e.strerror)
         exit(1)
 
     try:
-        with open(outfile, 'w') as fic:
+        with open(outfile, 'w') as file:
             ngrams = list(ngrams.items())
-            ngrams.sort(key=lambda elt : elt[1], reverse=True)
+            ngrams.sort(key=lambda element: element[1], reverse=True)
             for word, val in ngrams:
-                    fic.write(word + ' ' + str(val) + '\n')
+                    file.write(word + ' ' + str(val) + '\n')
     except Exception as e:
-        print('\nUne erreur est survenue lors de l\'écriture dans', outfile)
+        print('\nAn error occurred while writing ', outfile)
         print(e.strerror)
         exit(1)
 
     print(' Ok')
 
 def generate_stats(infile):
-    print('Lecture du fichier des n-grammes...', end='', flush=True)
+    print('Reading n-grams file...', end='', flush=True)
     ngrams = {}
     try:
-        with open(infile, 'r') as fic:
-            for line in fic:
+        with open(infile, 'r') as file:
+            for line in file:
                 words = line.split(' ')
                 value = int(words.pop())
                 next_word = words.pop()
@@ -89,7 +89,7 @@ def generate_stats(infile):
                 else:
                     ngrams[key] = { next_word : value }
     except Exception as e:
-        print('\nUne erreur est survenue lors de la lecture de', infile)
+        print('\nAn error occurred while reading ', infile)
         print(e.strerror)
         exit(1)
 
@@ -98,12 +98,12 @@ def generate_stats(infile):
         for next_words in ngrams[word]:
             n += ngrams[word][next_words]
         ngrams[word] = [ (next_words, round(ngrams[word][next_words] / n, 3)) for next_words in ngrams[word]]
-        ngrams[word].sort(key=lambda elt : elt[1], reverse=True)
+        ngrams[word].sort(key=lambda element : element[1], reverse=True)
 
     print(' Ok')
     return ngrams
 
-def predict(stats, term, threshold):
+def predict(stats, term, threshold = 0):
     result = []
     if term in stats:
         for words, value in stats[term]:
@@ -125,8 +125,17 @@ def high_predict(stats, threshold):
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print('Syntax : predict <corpus_file.txt>')
+        print('Syntax: predict <corpus_file.txt>')
         exit(2)
+
+    # unigrams(sys.argv[1])
+    # ngrams = generate_stats('unigrams.txt')
+    # print(predict(ngrams, 'th'))
+
     ngrams(sys.argv[1], 2)
     ngrams = generate_stats('2_grams.txt')
-    print(predict(ngrams, 'van', 0.1))
+    print(predict(ngrams, 'on'))
+
+    # ngrams(sys.argv[1], 3)
+    # ngrams = generate_stats('3_grams.txt')
+    # print(predict(ngrams, 'one of'))
